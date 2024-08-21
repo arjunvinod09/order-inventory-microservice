0de40bf2c9440734b78f55e99dadb943de988942
arjun.vinod <arjunvinod09@gmail.com>
2024-08-21 15:56:01 +0530

    added inventory feign service in order

--- order-service/src/main/java/com/ust/orderservice/feign_client/InventoryClientService.java
+++ order-service/src/main/java/com/ust/orderservice/feign_client/InventoryClientService.java
@@ -1,2 +1,13 @@
-package com.ust.orderservice.feign_client;public interface InventoryClientService {
+package com.ust.orderservice.feign_client;
+
+import com.ust.orderservice.payload.ResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "inventory",url = "http://localhost:8100/products")
+public interface InventoryClientService {
+
+    @GetMapping
+    ResponseDto isProductAvailable(@RequestParam String skuCode, @RequestParam int quantity);
 }
